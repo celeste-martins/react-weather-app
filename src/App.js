b6a0646d8886f8a25d597245060503382a845219
celeste-martins <celeste.martins@gmail.com>
2022-04-03 09:36:07 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Celeste Martins and{" "}
-        <a href="https://github.com/celeste-martins/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Celeste Martins and{" "}
+          <a href="https://github.com/celeste-martins/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
