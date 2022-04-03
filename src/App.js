6f14669e9e74e96bce57bf55dd2e40dca00cee62
celeste-martins <celeste.martins@gmail.com>
2022-04-03 09:16:46 -0300

    added footer open source

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Celeste Martins and{" "}
+        <a href="https://github.com/celeste-martins/react-weather-app">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
