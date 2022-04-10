7eea3790709574a6131e708925abac3b4e76d20a
celeste-martins <celeste.martins@gmail.com>
2022-04-10 09:19:50 -0300

    noreferrer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/celeste-martins/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
